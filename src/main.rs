eccf6e568e6af320adf99b3c12359ff956f5c7cd
Pratik Borole <borolepratik@gmail.com>
2025-02-16 19:30:23 +0530

    chore: apply rustfmt

--- src/main.rs
+++ src/main.rs
@@ -17,18 +17,17 @@ fn setup(mut commands: Commands) {
     commands.spawn(Camera2d::default());
 
     // Player
-    commands
-        .spawn((
-            Player,
-            Sprite {
-                color: Color::srgb(0.5, 1.0, 0.5),
-                custom_size: Some(Vec2::new(30.0, 50.0)),
-                anchor: Anchor::BottomCenter,
-                ..default()
-            },
-            Transform::from_xyz(PLAYER_X, GROUND_LEVEL, 0.0),
-            Velocity(Vec3::ZERO)
-        ));
+    commands.spawn((
+        Player,
+        Sprite {
+            color: Color::srgb(0.5, 1.0, 0.5),
+            custom_size: Some(Vec2::new(30.0, 50.0)),
+            anchor: Anchor::BottomCenter,
+            ..default()
+        },
+        Transform::from_xyz(PLAYER_X, GROUND_LEVEL, 0.0),
+        Velocity(Vec3::ZERO),
+    ));
 
     // Ground
     commands.spawn((
@@ -38,19 +37,20 @@ fn setup(mut commands: Commands) {
             anchor: Anchor::TopLeft,
             ..default()
         },
-        Transform::from_xyz(-400.0, GROUND_LEVEL, 0.0)
+        Transform::from_xyz(-400.0, GROUND_LEVEL, 0.0),
     ));
 }
 
 fn jump(
     mut events: EventReader<KeyboardInput>,
-    mut query: Query<(&mut Velocity, &Transform), With<Player>>
+    mut query: Query<(&mut Velocity, &Transform), With<Player>>,
 ) {
     for e in events.read() {
-        if let Ok((mut velocity, transform)) =
-            query.get_single_mut() {
-            if e.state.is_pressed() && e.key_code == KeyCode::Space
-                && transform.translation.y <= GROUND_LEVEL {
+        if let Ok((mut velocity, transform)) = query.get_single_mut() {
+            if e.state.is_pressed()
+                && e.key_code == KeyCode::Space
+                && transform.translation.y <= GROUND_LEVEL
+            {
                 velocity.0.y = JUMP_FORCE;
             }
         }
@@ -59,10 +59,9 @@ fn jump(
 
 fn player_movement(
     time: Res<Time>,
-    mut query: Query<(&mut Transform, &mut Velocity), With<Player>>
+    mut query: Query<(&mut Transform, &mut Velocity), With<Player>>,
 ) {
-    for (mut transform, mut velocity)
-    in query.iter_mut() {
+    for (mut transform, mut velocity) in query.iter_mut() {
         transform.translation.y += velocity.0.y * time.delta_secs();
         if transform.translation.y <= GROUND_LEVEL {
             transform.translation.y = GROUND_LEVEL;
@@ -71,10 +70,7 @@ fn player_movement(
     }
 }
 
-fn apply_gravity(
-    time: Res<Time>,
-    mut query: Query<&mut Velocity, With<Player>>
-) {
+fn apply_gravity(time: Res<Time>, mut query: Query<&mut Velocity, With<Player>>) {
     for mut velocity in query.iter_mut() {
         velocity.0.y += GRAVITY * time.delta_secs();
     }
